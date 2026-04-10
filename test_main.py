641c4de5a55f2a8bee26821df5329f3b60d42261
whitekotan <gcat5770@gmail.com>
2026-04-10 14:48:48 +0300

    chore: add test for read file and input keyword

---
+++ test_main.py
@@ -0,0 +1,16 @@
+import pytest
+from unittest.mock import mock_open, patch
+import main  
+
+
+def test_read_file():
+    mock_data = "fsferwewe \n  fsadq2312312"
+
+    with patch("builtins.open", mock_open(read_data=mock_data)):
+        result = main.read_file("fake.txt")
+
+    assert result == mock_data
+
+
+def test_input_keyword():
+    assert main.input_keyword("ab") == "ab"
