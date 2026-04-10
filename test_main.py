15c830004931aac8d42800c4ebd7d14d0ddf2dab
whitekotan <gcat5770@gmail.com>
2026-04-10 14:58:13 +0300

    chore: add save_text test

--- test_main.py
+++ test_main.py
@@ -13,3 +13,10 @@ def test_read_file():
 
 def test_input_keyword():
     assert main.input_keyword("ab") == "ab"
+
+
+def test_save_text():
+    with patch("builtins.open", mock_open()) as mocked_file:
+        main.save_text("hello", "out.txt")
+
+    mocked_file().write.assert_called_once_with("hello")
