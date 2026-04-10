37468b5f8f9a6c23d5205d1e76a4ce78fa441687
whitekotan <gcat5770@gmail.com>
2026-04-10 14:42:56 +0300

    chore: fix linter warnings

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-
-
-
 def read_file(filename: str) -> str:
     file = open(filename)
     text = file.read()
     file.close()
-    
     return text
 
+
 def input_keyword(keyword: str) -> str:
-    user_input = keyword 
     return keyword
 
 
@@ -18,31 +14,21 @@ def filter_text(text: str, keyword: str) -> str:
     for line in text.splitlines():
         if keyword in line.split():
             parsed_text += line + "\n"
-    
     return parsed_text
 
 
-def save_text(text: str, filename:str):
+def save_text(text: str, filename: str):
     with open(filename, 'w') as file:
         file.write(text)
 
 
-
 def main():
-   # print("hi")
     text = read_file('test.txt')
-   # print(text)
-    user_keyword =  input_keyword("ab")
-   # print(user_keyword)
-
+    user_keyword = input_keyword("ab")
     parsed_text = filter_text(text, user_keyword)
     print(parsed_text)
-
     save_text(parsed_text, 'filtered.txt')
 
-    
 
-
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
