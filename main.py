2702e5c412338f28e0b5888cb7406b74db05e4da
whitekotan <gcat5770@gmail.com>
2026-04-10 14:02:04 +0300

    feat: create func to read file

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 
 
 
+def read_file(filename: str) -> str:
+    file = open(filename)
+    text = file.read()
+    file.close()
+    print(text)
+    return text
+
+
+
 def main():
     print("hi")
+    text = read_file('test.txt')
+    print(text)
+
+    
 
 
 
